100aaf93ab573c992a4cc8f4511c4931d61b2b81
bahtirek <bahtirek@gmail.com>
2023-01-10 23:49:49 -0500

    prevent from opening few extensions at once

--- src/background.js
+++ src/background.js
@@ -1,64 +1,142 @@
-chrome.action.onClicked.addListener(buttonClicked);
-function buttonClicked(tab) {
-  chrome.tabs.sendMessage(tab.id, "inject");
-  chrome.scripting.executeScript({
+//browser.action.onClicked.addListener(buttonClicked);
+
+if (typeof browser === "undefined") {
+  var browser = chrome;
+}
+
+let lastWindowId;
+let activeTab;
+let extensionStatus = {
+  open: false,
+  location: '',
+  obj: {}
+};
+
+//save last focused window to get the screenshots
+browser.windows.getLastFocused(
+  null, (window)=>{
+    console.log('lastfoc', window);
+    lastWindowId = window.id
+  }
+)
+
+// save active tab, needed to inject the extension to active tab
+browser.tabs.onActivated.addListener((activeInfo) => {
+  browser.tabs.get(activeInfo.tabId, function (tab) {
+      activeTab = tab;
+  });
+});
+
+// Cleanup when extension window is closed
+browser.windows.onRemoved.addListener(onWindowRemoved);
+function onWindowRemoved(windowId){
+  if(!extensionStatus.open && !extensionStatus.location =='openedInNewWindow') return false;
+  if(windowId == extensionStatus.obj.id) {
+    extensionStatus = {
+      open: false,
+      location: '',
+      obj: {}
+    };
+  }
+}
+
+// Cleanup when extension tab is closed
+browser.tabs.onRemoved.addListener(onTabRemoved);
+function onTabRemoved(tabId){
+  if(!extensionStatus.open && !extensionStatus.location =='openedInNewTab') return false;
+  if(tabId == extensionStatus.obj.id) {
+    extensionStatus = {
+      open: false,
+      location: '',
+      obj: {}
+    };
+    //browser.tabs.onRemoved.removeListener(onWindowRemoved);
+  }
+}
+
+browser.runtime.onMessage.addListener(async function (request, sender, sendResponse) {
+
+  if (request.todo == 'openedInNewWindow') {
+    console.log(request);
+    extensionStatus = {
+      open: true,
+      location: 'openedInNewWindow',
+      obj: request.window
+    }
+  }
+
+  if (request.todo == 'openInTab') {
+    const newURL = "./index.html";
+    if(!extensionStatus.open) {
+      const response = await browser.tabs.create({ url: newURL });
+      if(response) {
+        extensionStatus = {
+          open: true,
+          location: 'openedInNewTab',
+          obj: response
+        };
+        sendResponse(true);
+      }
+    }
+  }
+
+  if (request.todo == 'openInPage') {
+    console.log(extensionStatus.open);
+    //change to promise
+    if(activeTab && !extensionStatus.open) {
+      buttonClicked(activeTab);
+      sendResponse(true);
+    }
+  }
+
+  if (request.todo == 'getImage') {
+    console.log(request.todo);
+    browser.tabs.captureVisibleTab(null, {format: 'png'}, (dataUrl) => {
+      sendResponse({imgSrc:dataUrl});
+    });
+ 
+    return true;
+  }
+})
+
+
+async function buttonClicked(tab) { //change to promise
+  await browser.tabs.sendMessage(tab.id, "inject");
+  await browser.scripting.executeScript({
     target: {tabId: tab.id},
     files: ['runtime.js']
   });
-  chrome.scripting.executeScript({
+  await browser.scripting.executeScript({
     target: {tabId: tab.id},
     files: ['polyfills.js']
   });
-  chrome.scripting.executeScript({
+  await browser.scripting.executeScript({
     target: {tabId: tab.id},
     files: ['main.js']
   });
   console.log('inject background');
 }
 
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  if (request.todo == 'getImage') {
-    console.log(request.todo);
-    chrome.tabs.captureVisibleTab(null, {format: 'png'}, (dataUrl) => {
-      sendResponse({imgSrc:dataUrl});
-    });
- 
-    return true;
-  }
-  if (request.todo == 'setAlarm') {
-    console.log(request.todo);
-    chrome.alarms.create('videoAlarm', {
-      delayInMinutes: 1
-    });
-    return true;
-  }
-  if (request.todo == 'clearAlarm') {
-    console.log(request.todo);
-    chrome.alarms.clearAll();
-    return true;
-  }
-})
-
-chrome.commands.onCommand.addListener((command) => {
+browser.commands.onCommand.addListener((command) => {
 
   //Cntr-Shift-S
   if (command === 'trigger_select') {
-    //chrome.tabs.executeScript(null, {file: 'component.js'});
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-      chrome.tabs.sendMessage(tabs[0].id, "trigger_select");  
+    //browser.tabs.executeScript(null, {file: 'component.js'});
+    browser.tabs.query({active: true, currentWindow: true}, function(tabs){
+      browser.tabs.sendMessage(tabs[0].id, "trigger_select");  
     });
   }
   
   //Cntr-Shift-U
   if (command === 'get_screenshot') {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-      chrome.tabs.sendMessage(tabs[0].id, "get_screenshot");  
+    browser.tabs.query({active: true, currentWindow: true}, function(tabs){
+      browser.tabs.sendMessage(tabs[0].id, "get_screenshot");  
     });
   }
 });
 
-chrome.alarms.onAlarm.addListener(() => {
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-    chrome.tabs.sendMessage(tabs[0].id, "stop-video-recording");  
+browser.alarms.onAlarm.addListener(() => {
+  browser.tabs.query({active: true, currentWindow: true}, function(tabs){
+    browser.tabs.sendMessage(tabs[0].id, "stop-video-recording");  
   });
 })
