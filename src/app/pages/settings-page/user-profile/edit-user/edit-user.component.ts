729f467bb1917507076c9060589f0c76cdd4d6a1
bahtirek <bahtirek@gmail.com>
2023-02-09 00:02:25 -0500

    edit user flow

--- src/app/pages/settings-page/user-profile/edit-user/edit-user.component.ts
+++ src/app/pages/settings-page/user-profile/edit-user/edit-user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { User } from 'src/app/interfaces/user.inteface';
 
 @Component({
@@ -8,11 +9,41 @@ import { User } from 'src/app/interfaces/user.inteface';
 })
 export class EditUserComponent implements OnInit {
 
-  constructor() { }
+  submitInProgress: boolean = false; 
+  submitClicked: boolean;
+
+  get firstname() {
+    return this.userForm.get('firstname');
+  }
+
+  get lastname() {
+    return this.userForm.get('lastname');
+  }
+
+  userForm: FormGroup = this.fb.group({
+    firstname: ['', [Validators.required, ]],
+    lastname: ['', [Validators.required, ]],
+  }); 
+
+  constructor(private fb: FormBuilder ) { }
 
   ngOnInit(): void {
   }
 
   @Input() user: User;
+  
+  onSubmit(){
+    this.submitClicked = true;
+    this.submitInProgress = true;
+    if(this.userForm.valid) {
+      console.log(this.userForm.value);
+      this.onCancel();
+    }
+  }
 
+  onCancel(){
+    this.userForm.reset();
+    this.submitClicked = false;
+    this.submitInProgress = false;
+  }
 }
