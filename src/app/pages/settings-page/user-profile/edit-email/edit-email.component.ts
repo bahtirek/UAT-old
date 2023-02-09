f55559bc0a5cbb1222306c609cfc99837c86c78e
bahtirek <bahtirek@gmail.com>
2023-02-09 00:02:48 -0500

    edit email flow

--- src/app/pages/settings-page/user-profile/edit-email/edit-email.component.ts
+++ src/app/pages/settings-page/user-profile/edit-email/edit-email.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { User } from 'src/app/interfaces/user.inteface';
 
 @Component({
   selector: 'app-edit-email',
@@ -7,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditEmailComponent implements OnInit {
 
-  constructor() { }
+  submitInProgress: boolean = false; 
+  submitClicked: boolean;
+
+  get email() {
+    return this.emailForm.get('email');
+  }
+
+
+  emailForm: FormGroup = this.fb.group({
+    email: ['', [Validators.required, Validators.pattern(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/) ]],
+  }); 
+
+  constructor(private fb: FormBuilder ) { }
 
   ngOnInit(): void {
+  }
+
+  @Input() user: User;
+  
+  onSubmit(){
+    this.submitClicked = true;
+    this.submitInProgress = true;
+    if(this.emailForm.valid) {
+      console.log(this.emailForm.value);
+      this.onCancel();
+    }
+  }
+
+  onCancel(){
+    this.emailForm.reset();
+    this.submitClicked = false;
+    this.submitInProgress = false;
   }
 
 }
