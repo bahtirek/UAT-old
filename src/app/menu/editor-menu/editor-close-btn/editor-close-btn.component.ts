b393e9f38ad06b32e2de71fc9ed66d26673634c3
bahtirek <bahtirek@gmail.com>
2023-02-27 22:31:53 -0500

    maybe will use editor drop menu

---
+++ src/app/menu/editor-menu/editor-close-btn/editor-close-btn.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-editor-close-btn',
+  templateUrl: './editor-close-btn.component.html',
+  styleUrls: ['./editor-close-btn.component.less']
+})
+export class EditorCloseBtnComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
